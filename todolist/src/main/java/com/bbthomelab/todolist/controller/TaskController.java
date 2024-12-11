6646f2ae31f7737a7a2dfa8f175450ba16e2f1e7
bambabt <bibamba05@gmail.com>
2024-12-11 17:40:21 -0600

    update service and controller classes

--- todolist/src/main/java/com/bbthomelab/todolist/controller/TaskController.java
+++ todolist/src/main/java/com/bbthomelab/todolist/controller/TaskController.java
@@ -1,5 +1,35 @@
 package com.bbthomelab.todolist.controller;
 
-public class TaskController {
+import com.bbthomelab.todolist.model.Task;
+import com.bbthomelab.todolist.service.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/tasks")
+public class TaskController {
+	@Autowired
+	private TaskService taskService;
+	
+	@GetMapping
+	public List<Task> getAllTasks() {
+		return taskService.getAllTasks();
+	}
+	
+	@PostMapping
+	public Task createTask(@RequestBody Task task) {
+		return taskService.addTask(task);
+	}
+	
+	@PutMapping("/{id}")
+	public Task updateTask(@PathVariable Long id, @RequestBody Task task) {
+		return taskService.updateTask(id, task);
+	}
+	
+	@DeleteMapping("/{id}")
+	public void deleteTask(@PathVariable Long id) {
+		taskService.deleteTask(id);
+	}
 }
